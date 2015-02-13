21f143836a0d699b5205b08e533d4792b729f900
Jonathan Morton <djlarrikin@gmail.com>
2015-02-13 16:06:04 +0900

    Lesson 32 Completed

--- app/src/main/java/com/bignerdranch/android/photogallery/PhotoPageFragment.java
+++ app/src/main/java/com/bignerdranch/android/photogallery/PhotoPageFragment.java
@@ -1,10 +1,15 @@
 package com.bignerdranch.android.photogallery;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 /**
  * Created by larrikin on 2/11/15.
@@ -20,12 +25,45 @@ public class PhotoPageFragment extends VisibleFragment {
 
         mUrl = getActivity().getIntent().getData().toString();
     }
-
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_photo_page, parent, false);
 
+        final ProgressBar progressBar =
+                (ProgressBar)v.findViewById(R.id.progressBar);
+        progressBar.setMax(100);
+        final TextView titleTextView =
+                (TextView)v.findViewById(R.id.titleTextView);
+
         mWebView = (WebView)v.findViewById(R.id.webView);
+
+        mWebView.getSettings().setJavaScriptEnabled(true);
+
+        mWebView.setWebViewClient(new WebViewClient(){
+            public boolean shouldOverrideUrlLoading(WebView view, String url){
+                return false;
+            }
+        });
+
+        mWebView.setWebChromeClient(new WebChromeClient(){
+            @Override
+            public void onProgressChanged(WebView view, int progress) {
+                if (progress == 100){
+                    progressBar.setVisibility(View.INVISIBLE);
+                } else {
+                    progressBar.setVisibility(View.VISIBLE);
+                    progressBar.setProgress(progress);
+                }
+            }
+
+            @Override
+            public void onReceivedTitle(WebView view, String title) {
+                titleTextView.setText(title);
+            }
+        });
+
+        mWebView.loadUrl(mUrl);
 
         return v;
     }
